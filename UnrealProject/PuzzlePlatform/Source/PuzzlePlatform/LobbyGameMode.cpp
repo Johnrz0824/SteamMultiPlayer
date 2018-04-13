f6ef03e389197d224f8cc31e1b055b004494c29a
Hector Hung <johnrz0824@gmail.com>
2018-04-13 14:14:40 +0800

    67

--- UnrealProject/PuzzlePlatform/Source/PuzzlePlatform/LobbyGameMode.cpp
+++ UnrealProject/PuzzlePlatform/Source/PuzzlePlatform/LobbyGameMode.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "LobbyGameMode.h"
+#include "Engine/World.h"
 
 
 
@@ -10,6 +11,13 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 	Super::PostLogin(NewPlayer);
 	players++;
 	UE_LOG(LogTemp,Warning,TEXT("New Player!!!  %d"),players)
+	if (players >= 2)
+	{
+		UWorld* world = GetWorld();
+		if (!ensure(world != nullptr))return;
+		bUseSeamlessTravel = true;
+		world->ServerTravel("/Game/PuzzlePlatform/Game?listen");
+	}
 }
 
 void ALobbyGameMode::Logout(AController* Exiting)
